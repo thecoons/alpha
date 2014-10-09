41c4f293d328f71e4f8099a3fdf8eb8faa3e1ecf
thecoons <antobarth@gmail.com>
2014-10-09 12:11:24 +0200

    Architecture Projet Vierge

---
+++ src/fr/kernel/alpha/AlphaServlet.java
@@ -0,0 +1,11 @@
+package fr.kernel.alpha;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class AlphaServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
